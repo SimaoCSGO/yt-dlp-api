6dbf09f81c51fd6db4421878c99fe1e39efd512d
SimaoCSGO <simaosoares111@gmail.com>
2025-05-20 11:26:46 +0100

    Add files via upload

---
+++ server.py
@@ -0,0 +1,30 @@
+from flask import Flask, request, jsonify
+from yt_dlp import YoutubeDL
+
+app = Flask(__name__)
+
+@app.route('/search', methods=['GET'])
+def search():
+    query = request.args.get('q')
+    ydl_opts = {
+        'format': 'bestaudio/best',
+        'noplaylist': True,
+        'quiet': True
+    }
+
+    with YoutubeDL(ydl_opts) as ydl:
+        info = ydl.extract_info(f"ytsearch1:{query}", download=False)['entries'][0]
+        return jsonify({
+            'title': info['title'],
+            'url': info['url'],
+            'webpage_url': info['webpage_url'],
+            'thumbnail': info.get('thumbnail'),
+            'audio_url': info['url']
+        })
+
+@app.route('/')
+def root():
+    return "Servidor yt-dlp está online."
+
+if __name__ == '__main__':
+    app.run()
